c37ec8ba075e6004bc402e342fd1f2b1a1d3cccc
floriangtmegmail <floriangtme@gmail.com>
2023-05-18 19:50:38 +0200

    fonctionalité ok new branch

--- public/asset/js/appRealisation.js
+++ public/asset/js/appRealisation.js
@@ -1,15 +1,25 @@
 
+const titleProjetRealiz = document.querySelector('#title-projet-realiz')
 const tableTdLanguage = document.querySelector('#table-td-language')
 const tableTdFramwork = document.querySelector('#table-td-framework')
+const tableTdSgbd = document.querySelector('#table-td-sgbd')
+
+
 const divRealizDiagrame = document.querySelector('#div-realiz-diagrame')
+
+
+const listBtnShowRealiz = document.querySelectorAll('.btn-show-realiz')
+
+
 
 const testReal = document.querySelector('#testReal')
 
 
-function viewAchievementTable(folderImg, data, size) {
+function viewAchievementTableImg(folderImg, data, minefile, size) {
 
+    
     return `
-        <img class="pt-4 m-1" src="../public/img/${folderImg}/${data.name}_logo.svg" alt="${data.name}" title="${data.name}"
+        <img class="m-3" src="../public/img/${folderImg}/${data}_logo.${minefile}" alt="${data}" title="${data}"
         style="width: ${size};">
     `
 }
@@ -36,11 +46,11 @@ function checkACookieExists() {
     if (document.cookie.split(";").some((item) => item.trim().startsWith(`${clef}=`))) {
         cookieValue = document.cookie.split(";").find((row) => row.startsWith(`${clef}=`))?.split("=")[1];
 
-        console.log(cookieValue);
+        // console.log(cookieValue);
 
 
         fetchJson('./../public/asset/js/datas.json').then((datas) => {
-            console.log(datas.realisation);
+            // console.log(datas.realisation);
 
             let dataRealisation = datas.realisation
 
@@ -65,37 +75,75 @@ function eachDataRender(datas, cookieValue) {
     datas.forEach(element => {
 
         if (element.title === cookieValue) {
-            console.log(element);
-            let textContent = element.content
-            let contentSplit = textContent.split('. ');
-            contentSplit.forEach((elm) => {
-                addElmt("#text-content-realiz", "p", "resum-projet-text", elm)
-            })
+            // console.log(element);
 
+            titleProjetRealiz.textContent = element.title;
+            
+            let elementContent = element.content
+
+            addNodeElementWhoContent("#text-content-realiz", "p", "resum-projet-text", elementContent)
 
             element.language.forEach(el => {
-                console.log(el.name);
-                chaine += viewAchievementTable('language', el, '3rem');
+                // console.log(el.name);
+                chaine += viewAchievementTableImg('language', el.name,'svg', '3rem');
                 tableTdLanguage.innerHTML = chaine
             });
 
 
             element.frameWork.forEach(el => {
-                console.log(el.name);
-                chaine2 += viewAchievementTable('framework', el, '10rem');
+                // console.log(el.name);
+                chaine2 += viewAchievementTableImg('framework', el.name,'svg', '8rem');
                 tableTdFramwork.innerHTML = chaine2
             });
 
             element.analyse.forEach(el => {
-                console.log(el.nameFile);
+                // console.log(el.nameFile);
                 chaine3 += viewAchievDiagram(el)
                 divRealizDiagrame.innerHTML = chaine3
             });
 
-
+            console.log(element.bataBase);
+            tableTdSgbd.innerHTML = viewAchievementTableImg('tools',element.bataBase,'png', '5rem');
+            
 
         }
     });
+
+}
+
+
+listBtnShowRealiz.forEach((elmBtn) => {
+    elmBtn.addEventListener('click', (event) => {
+        // console.log(event.currentTarget);
+        // console.log(event.currentTarget.textContent);
+
+        addCookie(event.currentTarget.textContent)
+        checkACookieExists()
+
+    })
+})
+
+
+function addNodeElementWhoContent(elmIdParent, noeudCre, classNameParam, newContent) {
+
+    let txt = "";
+
+    let elmAddParent = document.querySelector(elmIdParent);
+
+    let newNoeud = document.createElement(noeudCre.toUpperCase());
+    newNoeud.setAttribute('class', classNameParam);
+
+    let contentSplit = newContent.split('. ');
+
+    if (contentSplit.length > 0) {
+
+        contentSplit.forEach((elm) => {
+
+            txt += `<p>${elm}</p>`
+
+        })
+        elmAddParent.innerHTML = txt
+    }
 
 }
 
